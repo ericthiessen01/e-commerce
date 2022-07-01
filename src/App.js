a722078755e496e0cd8b8be2dfcf34449ed2f12a
ericthiessen01 <ericthiessen01@gmail.com>
2022-06-30 23:40:28 -0600

    added tailwind and nav

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-//   Route,
-//   Link
-// } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom'
+import Nav from './components/Nav'
+import Home from './pages/Home'
+import About from './pages/About'
+import Cart from './pages/Cart'
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path='/e-commerce/' element={<Home />}/>
+          <Route path='/e-commerce/About' element={<About />}/>
+          <Route path='/e-commerce/Cart' element={<Cart />}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 
 export default function Nav() {
-  return (
-    <div>Nav</div>
-  )
+    let navigate = useNavigate()
+
+    return (
+        <div className='nav-container flex justify-between items-center'>
+            <div className='nav-links flex'>
+                <div className='link text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-lg px-5 py-2.5 text-center mr-2 mb-2' onClick={() => {navigate("/e-commerce/")}} >Home</div>
+                <div className='link text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-lg px-5 py-2.5 text-center mr-2 mb-2' onClick={() => {navigate("/e-commerce/About")}} >About</div>
+                <div className='link text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-lg px-5 py-2.5 text-center mr-2 mb-2' onClick={() => {navigate("/e-commerce/Cart")}} >Check-out</div>
+            </div>
+            <FontAwesomeIcon icon={faCartShopping} size="5x" className='text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-2xl px-5 py-2.5 text-center mr-2 mb-2'/>
+        </div>
+    )
 }
