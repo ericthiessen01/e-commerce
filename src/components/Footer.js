f7222177a55503b385dddfd8819306a49781a65c
ericthiessen01 <ericthiessen01@gmail.com>
2022-07-03 19:22:09 -0600

    footer added

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='h-[5vh] text-xs bg-stone-50 p-1 shadow-[0px_-5px_15px_-8px_rgba(0,0,0,0.3)]'>
+        <p>Things & Stuff Store</p>
+        <p>Data courtesy of <a href='https://fakestoreapi.com/' target='_blank'>https://fakestoreapi.com/</a></p>
+    </div>
+  )
+}
